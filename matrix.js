d64be3fe05cbfddcbb14737987c2aab475bae132
LevanNamicheishvili <117360031+LevanNamicheishvili@users.noreply.github.com>
2023-10-10 01:12:54 +0400

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,1 +1,20 @@
+const generateRandomMatrix = (rows, columns) => {
+    const matrix = Array.from({ length: rows }, () =>
+        Array.from({ length: columns }, () => Math.floor(Math.random() * 10))
+    );
+    return matrix;
+};
+
+const printMatrix = (matrix) => {
+    matrix.forEach(row => console.log(row.join('\t')));
+};
+
+
+const rows = Math.floor(Math.random() * 5) + 1;
+const columns = Math.floor(Math.random() * 5) + 1;
+
+const randomMatrix = generateRandomMatrix(rows, columns);
+
+console.log(`Random Matrix with ${rows} rows and ${columns} columns:`);
+printMatrix(randomMatrix);
 
